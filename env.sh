9886e017eb3192c4787a6df6b48a78c0d8518683
Heiko Thiery <heiko.thiery@kontron.com>
2019-09-23 10:30:27 +0200

    fix
    Signed-off-by: Heiko Thiery <heiko.thiery@kontron.com>

--- env.sh
+++ env.sh
@@ -1,3 +1,3 @@
 export PATH=$PATH:$(pwd)/rootfs/bin/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/rootfs/lib
-export PKG_CONFIG_PATH="$(pwd)/rootfs/lib/pkgconfig"
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$(pwd)/rootfs/lib/pkgconfig

b2fe16d9f091b00b6c594989dd8314cda2a60329
Heiko Thiery <heiko.thiery@kontron.com>
2019-10-21 10:50:25 +0200

    change rootfs to sysroot
    Signed-off-by: Heiko Thiery <heiko.thiery@kontron.com>

--- env.sh
+++ env.sh
@@ -1,3 +1,5 @@
-export PATH=$(pwd)/rootfs/bin/:${PATH}
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/rootfs/lib
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$(pwd)/rootfs/lib/pkgconfig
+SYSROOT=$(pwd)/sysroot/
+
+export PATH=${SYSROOT}/bin/:${PATH}
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${SYSROOT}/lib
+export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${SYSROOT}/lib/pkgconfig

6ff9f5282a6028c9187b203f7527ec2c03af88f3
Heiko Thiery <heiko.thiery@kontron.com>
2019-10-21 10:47:11 +0200

    changed PATH order
    Signed-off-by: Heiko Thiery <heiko.thiery@kontron.com>

--- env.sh
+++ env.sh
@@ -1,3 +1,3 @@
-export PATH=$PATH:$(pwd)/rootfs/bin/
+export PATH=$(pwd)/rootfs/bin/:${PATH}
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/rootfs/lib
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$(pwd)/rootfs/lib/pkgconfig

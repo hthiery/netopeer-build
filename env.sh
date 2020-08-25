ce9b8a0428262d9844c2c009e85063cc784162fb
Heiko Thiery <heiko.thiery@kontron.com>
2020-08-25 21:50:00 +0200

    env.sh: fix pathes
    Remove unneeded '/'.
    
    Signed-off-by: Heiko Thiery <heiko.thiery@kontron.com>

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
-SYSROOT=$(pwd)/sysroot/
+SYSROOT=$(pwd)/sysroot
 
-export PATH=${SYSROOT}/bin/:${PATH}
+export PATH=${SYSROOT}/bin:${PATH}
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${SYSROOT}/lib
 export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${SYSROOT}/lib/pkgconfig
